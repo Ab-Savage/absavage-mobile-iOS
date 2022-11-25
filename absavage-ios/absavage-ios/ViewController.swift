aeb485ac96de042f9e45b20b9ecc9127e379f469
Puras Handharmahua <puras.handharmahua@mekari.com>
2022-11-26 00:16:49 +0700

    initiate Absavage project

---
+++ absavage-ios/absavage-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  absavage-ios
+//
+//  Created by MEKARI on 26/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
